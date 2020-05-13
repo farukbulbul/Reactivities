4262b7153a6317fde40c4eb13a39226209347fa0
Ömer Faruk BÜLBÜL <farukbulbul@gmail.com>
2020-05-14 00:51:53 +0300

    end of section 7

--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -1,7 +1,7 @@
 export interface IActivity {
     id: string;
     title: string;
-    description:string;
+    description: string;
     category: string;
     date: string;
     city: string;
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,18 +1,11 @@
-import React from "react";
-import { Card, Image, Button } from "semantic-ui-react";
-import { IActivity } from "../../../app/models/activity";
+import React, { useContext } from 'react';
+import { Card, Image, Button } from 'semantic-ui-react';
+import ActivityStore from '../../../app/stores/activityStore';
+import { observer } from 'mobx-react-lite';
 
-interface IProps {
-  activity: IActivity;
-  setEditMode: (editMode: boolean) => void;
-  setSelectedActivity: (activity: IActivity | null) => void;
-}
-
-export const ActivityDetails: React.FC<IProps> = ({
-  activity,
-  setEditMode,
-  setSelectedActivity,
-}) => {
+const ActivityDetails: React.FC = () => {
+  const activityStore = useContext(ActivityStore);
+  const { selectedActivity: activity, openEditForm, cancelSelectedActivity } = activityStore;
   return (
     <Card fluid>
       <Image
@@ -21,28 +14,30 @@ export const ActivityDetails: React.FC<IProps> = ({
         ui={false}
       />
       <Card.Content>
-        <Card.Header>{activity.title}</Card.Header>
+        <Card.Header>{activity!.title}</Card.Header>
         <Card.Meta>
-          <span>{activity.date}</span>
+          <span>{activity!.date}</span>
         </Card.Meta>
-        <Card.Description>{activity.description}</Card.Description>
+        <Card.Description>{activity!.description}</Card.Description>
       </Card.Content>
       <Card.Content extra>
         <Button.Group widths={2}>
           <Button
-            onClick={() => setEditMode(true)}
+            onClick={() => openEditForm(activity!.id)}
             basic
-            color="blue"
-            content="Edit"
+            color='blue'
+            content='Edit'
           />
           <Button
-            onClick={() => setSelectedActivity(null)}
+            onClick={cancelSelectedActivity}
             basic
-            color="grey"
-            content="Cancel"
+            color='grey'
+            content='Cancel'
           />
         </Button.Group>
       </Card.Content>
     </Card>
   );
 };
+
+export default observer(ActivityDetails);
